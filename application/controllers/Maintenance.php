3555516f0caa5ec8764926066e361ea80a8778d5
Matt Goerwell <superninja246@hotmail.com>
2017-04-06 14:58:12 -0700

    Remaining REST/CRUD support

--- application/controllers/Maintenance.php
+++ application/controllers/Maintenance.php
@@ -41,21 +41,37 @@ class Maintenance extends Rest_Controller {
             $this->response(array('error' => 'Menu item not found!'), 404);        
     }
 
-    // Handle an incoming PUT - crUd
+    // Handle an incoming PUT - update a menu item
     function index_put()
     {
-        $this->response('ok', 200);
+        $key = $this->get('id');
+        $record = array_merge(array('id' => $key), $this->_put_args);
+        $this->menu->update($record);
+        $this->response(array('ok'), 200);
     }
 
-    // Handle an incoming POST - Crud
+    // Handle an incoming POST - add a new menu item
     function index_post()
     {
-        $this->response('ok', 200);
+        $key = $this->get('id');
+        $record = array_merge(array('id' => $key), $_POST);
+        $this->menu->add($record);
+        $this->response(array('ok'), 200);
     }
 
-    // Handle an incoming DELETE - cruD
+    // Handle an incoming DELETE - delete a menu item
     function index_delete()
     {
-        $this->response('ok', 200);
+        $key = $this->get('id');
+        $this->menu->delete($key);
+        $this->response(array('ok'), 200);
+    }
+    
+    // Handle an incoming DELETE - delete a menu item
+    function item_delete()
+    {
+        $key = $this->get('id');
+        $this->menu->delete($key);
+        $this->response(array('ok'), 200);
     }
 }
