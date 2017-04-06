7aa132d8d0188495ec4aa75663ef94380accc017
Matt Goerwell <superninja246@hotmail.com>
2017-04-06 14:52:22 -0700

    Initial REST support (get)

---
+++ application/controllers/Maintenance.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * REST server for menu operations.
+ * This one manages .
+ *
+ * ------------------------------------------------------------------------
+ */
+require APPPATH . '/third_party/restful/libraries/Rest_controller.php';
+class Maintenance extends Rest_Controller {
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('menu');
+	}
+
+    // Handle an incoming GET ... return a menu item or all of them
+    function index_get()
+    {
+        $key = $this->get('id');
+        if (!$key)
+        {
+            $this->response($this->menu->all(), 200);
+        } else
+        {
+            $result = $this->menu->get($key);
+            if ($result != null)
+                $this->response($result, 200);
+            else
+                $this->response(array('error' => 'Menu item not found!'), 404);
+        }
+    }
+    
+    // Handle an incoming GET ... return 1 menu item
+    function item_get()
+    {
+        $key = $this->get('id');
+        $result = $this->menu->get($key);
+        if ($result != null)
+            $this->response($result, 200);
+        else
+            $this->response(array('error' => 'Menu item not found!'), 404);        
+    }
+
+    // Handle an incoming PUT - crUd
+    function index_put()
+    {
+        $this->response('ok', 200);
+    }
+
+    // Handle an incoming POST - Crud
+    function index_post()
+    {
+        $this->response('ok', 200);
+    }
+
+    // Handle an incoming DELETE - cruD
+    function index_delete()
+    {
+        $this->response('ok', 200);
+    }
+}
